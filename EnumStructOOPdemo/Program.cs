16ac364ed940b7122c53024c547471b73a0e65b5
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-12 09:12:22 +0100

    Add project files.

---
+++ EnumStructOOPdemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumStructOOPdemo
+{
+    public enum Subjects
+    {
+        CSharp,
+        Java,
+        Networking,
+        Taal
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+    public struct Student
+    {
+        public string Name;
+        public int Age;
+        public Subjects Subject;
+
+        public Student()
+        {
+
+        }
+    }
+}
